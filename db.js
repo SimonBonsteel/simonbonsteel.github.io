15e6163d68fc6ea3aa81ba3dfb9a4f8c63f6d0d9
caeserlettuce <caeserlettuce@gmail.com>
2024-06-15 13:24:17 -0700

    made it all good and stuff
    yknow how it is

--- db.js
+++ db.js
@@ -1,17 +1,101 @@
 var db = {
 
     "collections": [
+
         {
             "title": "planes of flying",
             "description": "they be goin so high",
             "cover": 0,
+            "youtube": false,
             "contents": [
+                
+                
                 {
-                    "title": "",
+                    "title": "hdksjugfbdxhjkgbld",
                     "description": "the planes be flying",
-                    "src": "assets/background/_1230004.jpg"
-                }
+                    "src": "assets/background/_1230004.jpg",
+                    "youtube": false
+                },
+                {
+                    "title": "hdksjugfbdxhjkgbld",
+                    "description": "the planes be flying",
+                    "src": "assets/background/_1370407.jpg",
+                    "youtube": "C0cCKmCj8ew"
+                }   
 
+            ]
+        },
+        {
+            "title": "william osman if he was a cat",
+            "description": "opensauce more like opensus",
+            "cover": 0,
+            "youtube": "https://www.youtube.com/watch?v=jzdkPfct8N4",
+            "contents": [
+                {   // this is photo #0         NOTE: this entry is not shown, the website only uses the 'src' for the thumbnail of the video
+                    "src": "assets/background/_1370407.jpg",
+                },
+            ]
+        }
+
+    //  ^ this is where you'll add new collections
+
+
+    ]
+
+
+}
+
+
+
+/*
+       ______  ______  __  ___  ____    __      ___      ______  ______      ____    ____   
+      /_  __/ / ____/ /  |/  / / __ \  / /     /   |    /_  __/ / ____/     / __ \  / __ ) 
+       / /   / __/   / /|_/ / / /_/ / / /     / /| |     / /   / __/       / / / / / __  |
+      / /   / /___  / /  / / / ____/ / /___  / ___ |    / /   / /___      / /_/ / / /_/ /
+     /_/   /_____/ /_/  /_/ /_/     /_____/ /_/  |_|   /_/   /_____/     /_____/ /_____/
+*/
+// this is a TEMPLATE !!!!! im making this template for YOU simon !!!
+// im making it so that you can refer to it when adding photos if you forget how it works
+
+var TEMPLATE_DB = {
+    
+    "collections": [    // this is the list of collections, all collections go inside this (duh)
+
+        {   // this is a basic normal collection
+            "title": "planes of flying",            // title obv
+            "description": "they be goin so high",  // description obv
+            "cover": 0,                             // this defines what image will be used as the cover image of this collection. it's set to 0, so it'll use image #0 from the contents list below.
+            "youtube": false,                       // this is if there is a standalone video that doesn't have photos to go with it (ex. a showreel). replace 'false' with a the link to the youtube video surrounded in double quotes (ex. "https://www.youtube.com/watch?v=dQw4w9WgXcQ" )
+            "contents": [   // here is the list of photos
+                
+                {   // this is photo #0
+                    "title": "hdksjugfbdxhjkgbld",              // title obv
+                    "description": "the planes be flying",      // description obv
+                    "src": "assets/background/_1230004.jpg",    // path to the image file starting with assets/   also if this is going to be a youtube video, you want this path to go to what image you want for the thumbnail (the thumbnail MUST be landscape 3:2 aspect ratio)
+                    "youtube": false                            // if you want it to be a youtube video, again, replace 'false' with the video link, surrounded in double quotes.
+                },
+
+                {   // this is photo #1   (example of a youtube video)
+                    "title": "hdksjugfbdxhjkgbld",
+                    "description": "the planes be flying",
+                    "src": "assets/background/_1370407.jpg",                    // the thumbnail should be your photo of the cat
+                    "youtube": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"    // link to youtube video
+                }   
+
+            ]
+        },
+
+
+        {   // this is an example of a standalone video (like what i mentioned above!)
+            "title": "richard rollman",
+            "description": "rolling",
+            "cover": 0,                                                 // for youtube videos, keep this at 0
+            "youtube": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",   // again the youtube link
+            "contents": [
+
+                {   // this is photo #0         NOTE: this entry is not shown, the website only uses the 'src' for the thumbnail of the video
+                    "src": "assets/background/_1370407.jpg",
+                },
             ]
         }
 
@@ -21,6 +105,7 @@ var db = {
 
 
     ]
+
 
 
 }
--- main.js
+++ main.js
@@ -16,33 +16,61 @@ if (windowWidth < windowHeight) {
 
 
 
+// the other funny
+var tag = document.createElement('script');
+tag.id = 'yt-script';
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+
 function close_collection() {
     document.querySelector(".collection-window-wrapper").classList.add("hidden");
 }
 
 function close_photo() {
+    document.querySelector("iframe.preview").setAttribute("src", "");
     document.querySelector(".photo-preview").classList.add("hidden");
 }
 
 function show_collection(cnum) {
-    var collection_elems = document.querySelector(".collection-window .list").children
-    
+    var collection_elems = document.querySelectorAll(".collection-window .list .template");
+    var collection_templates = [];
+
     for (i in collection_elems) {
         if (collection_elems[i].nodeType) {
-            if (collection_elems[i].classList.contains("template") == false) {
-                collection_elems[i].remove();
-            }
+            console.debug(collection_elems[i]);
+            collection_templates.push(collection_elems[i].cloneNode(true));
         }
+    }
+    
+    document.querySelector(".collection-window .list").innerHTML = "";
+
+    for (i in collection_templates) {
+        document.querySelector(".collection-window .list").appendChild(collection_templates[i]);
     }
 
     for (i in db["collections"][cnum]["contents"]) {
-        var node = document.querySelector(".collection-window .list .card.template").cloneNode(true);
-        node.classList.remove("template");
-        node.classList.remove("hidden");
-        node.querySelector("img").setAttribute("src", `${db["collections"][cnum]["contents"][i]["src"]}`);
-        node.setAttribute("onclick", `show_photo(${cnum}, ${i})`);
+        if (db["collections"][cnum]["contents"][i]["youtube"] == false) {
+            var node = document.querySelector(".collection-window .list .card.image.template").cloneNode(true);
+            node.classList.remove("template");
+            node.classList.remove("hidden");
+            node.querySelector("img").setAttribute("src", `${db["collections"][cnum]["contents"][i]["src"]}`);
+            node.setAttribute("onclick", `show_photo(${cnum}, ${i})`);
+    
+            document.querySelector(".collection-window .list").appendChild(node);
+        } else {
+            var node = document.querySelector(".collection-window .list .card.video.template").cloneNode(true);
+            node.classList.remove("template");
+            node.classList.remove("hidden");
+            node.querySelector(".img-wrapper").style = `background-image: url("${db["collections"][cnum]["contents"][i]["src"]}");`;
+            node.setAttribute("onclick", `show_photo(${cnum}, ${i})`);
 
-        document.querySelector(".collection-window .list").appendChild(node);
+            document.querySelector(".collection-window .list").appendChild(node);
+        }
+
+        
     }
 
     document.querySelector(".collection-window h1").innerHTML = `${db["collections"][cnum]["title"]}`;
@@ -53,11 +81,55 @@ function show_collection(cnum) {
 
 function show_photo(cnum, pnum) {
 
-    document.querySelector(".photo-preview img.preview").setAttribute("src", `${db["collections"][cnum]["contents"][pnum]["src"]}`);
-    document.querySelector(".photo-preview h1").innerHTML = `${db["collections"][cnum]["contents"][pnum]["title"]}`;
-    document.querySelector(".photo-preview h2").innerHTML = `${db["collections"][cnum]["contents"][pnum]["description"]}`;
+    var preview_json = db["collections"][cnum]["contents"][pnum]
 
-    document.querySelector(".photo-preview").classList.remove("hidden");
+    if (pnum == false) {
+        preview_json = db["collections"][cnum];
+    }
+
+    if (preview_json["youtube"] == false) {
+        document.querySelector(".photo-preview img.preview").setAttribute("src", `${preview_json["src"]}`);
+        document.querySelector(".photo-preview h1").innerHTML = `${preview_json["title"]}`;
+        document.querySelector(".photo-preview h2").innerHTML = `${preview_json["description"]}`;
+    
+        document.querySelector("iframe.preview").style.display = "none";
+        document.querySelector("img.preview").style.display = "";
+
+        document.querySelector(".photo-preview").classList.remove("hidden");
+    } else {
+
+        var youtube_link = preview_json["youtube"];
+
+        if (youtube_link.includes("youtu.be")) {
+            // https://youtu.be/dQw4w9WgXcQ?si=MxgsZK3iRidtbKww
+
+            var url_tm = new URL(youtube_link);
+            youtube_link = url_tm.pathname.replace("/", "");
+        } else if (youtube_link.includes("/watch")) {
+            // https://www.youtube.com/watch?v=dQw4w9WgXcQ
+
+            var url_tm = new URL(youtube_link);
+            var params = new URLSearchParams(url_tm.search);
+            youtube_link = params.get("v");
+        }
+
+        console.log(youtube_link);
+
+        // https://www.youtube-nocookie.com/embed/${config["bg video"]}?autoplay=1&loop=1&rel=0&enablejsapi=1
+        var iframe_node = document.querySelector("iframe.preview");
+        iframe_node.setAttribute("src", `https://www.youtube-nocookie.com/embed/${youtube_link}?autoplay=1&loop=1&rel=0&enablejsapi=1`);
+        iframe_node.setAttribute("frameborder", "0");
+
+        document.querySelector(".photo-preview h1").innerHTML = `${preview_json["title"]}`;
+        document.querySelector(".photo-preview h2").innerHTML = `${preview_json["description"]}`;
+
+        document.querySelector("iframe.preview").style.display = "";
+        document.querySelector("img.preview").style.display = "none";
+
+        document.querySelector(".photo-preview").classList.remove("hidden");
+        
+    }
+    
 
 }
 
@@ -80,13 +152,49 @@ document.addEventListener("keyup", (event) => {
 
 for (i in db["collections"]) {
 
-    var node = document.querySelector(".collections-taste .list .card.template").cloneNode(true);
-    node.classList.remove("template");
-    node.classList.remove("hidden");
-    node.querySelector("h2").innerHTML = `${db["collections"][i]["title"]}`;
-    node.querySelector("img").setAttribute("src", `${db["collections"][i]["contents"][db["collections"][i]["cover"]]["src"]}`);
-    node.setAttribute("onclick", `show_collection(${i})`);
+    if (db["collections"][i]["youtube"] == false) {
+        var node = document.querySelector(".collections-taste .list .card.image.template").cloneNode(true);
+        node.classList.remove("template");
+        node.classList.remove("hidden");
+        node.querySelector("h2").innerHTML = `${db["collections"][i]["title"]}`;
+        node.querySelector("img").setAttribute("src", `${db["collections"][i]["contents"][db["collections"][i]["cover"]]["src"]}`);
+        node.setAttribute("onclick", `show_collection(${i})`);
+        
+        document.querySelector(".collections-taste .list").appendChild(node);
+    } else {
+        var node = document.querySelector(".collections-taste .list .card.video.template").cloneNode(true);
+        node.classList.remove("template");
+        node.classList.remove("hidden");
+        node.querySelector("h2").innerHTML = `${db["collections"][i]["title"]}`;
+        node.querySelector(".img-wrapper").style = `background-image: url("${db["collections"][i]["contents"][db["collections"][i]["cover"]]["src"]}");`;
+        node.setAttribute("onclick", `show_photo(${i}, false)`);
+        
+        document.querySelector(".collections-taste .list").appendChild(node);
+    }
     
-    document.querySelector(".collections-taste .list").appendChild(node);
 
 }
+
+
+
+
+function update_scrollbar() {
+    var bio_rects = document.querySelector(".bio").getClientRects()[0];
+    
+    if (bio_rects.y > 0) {
+        document.querySelector(".sc-bio").style.top = `${bio_rects.y}px`;    
+    } else {
+        document.querySelector(".sc-bio").style.top = `0px`;    
+    }
+    
+}
+
+update_scrollbar()
+
+document.addEventListener('resize', () => {
+    update_scrollbar();
+})
+
+document.querySelector("main").addEventListener('scroll', () => {
+    update_scrollbar();
+})
